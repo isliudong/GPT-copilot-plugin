27aa3022b7612fc7debd7b2344a8735e41120de6
刘冬 <2351036454@qq.com>
2023-03-19 02:49:38 +0800

    add Configuration Interaction Optimization

--- src/main/java/com/ld/chatgptcopilot/ui/ChatGPTCopilotToolWindowFactory.java
+++ src/main/java/com/ld/chatgptcopilot/ui/ChatGPTCopilotToolWindowFactory.java
@@ -1,16 +1,23 @@
 package com.ld.chatgptcopilot.ui;
 
 
+import javax.swing.*;
+
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.openapi.wm.ToolWindowType;
+import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.intellij.ui.content.ContentManager;
+import com.ld.chatgptcopilot.persistent.ChatGPTCopilotServerManager;
+import com.ld.chatgptcopilot.ui.dialog.ConfigureChatGptDialog;
 import com.ld.chatgptcopilot.ui.panel.AiCopilotPanel;
+import com.ld.chatgptcopilot.util.ChatGPTCopilotPanelUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class ChatGPTCopilotToolWindowFactory implements ToolWindowFactory, DumbAware {
@@ -24,9 +31,18 @@ public class ChatGPTCopilotToolWindowFactory implements ToolWindowFactory, DumbA
         this.toolWindow = toolWindow;
         createContent(project);
         toolWindow.setType(ToolWindowType.DOCKED, null);
+        ChatGPTCopilotServerManager.getInstance().addConfigurationServerChangedListener(() -> {
+            SwingUtilities.invokeLater(() -> createContent(project));
+        });
     }
 
     private void createContent(Project project) {
+        if (ChatGPTCopilotServerManager.getInstance().getAPIToken() == null) {
+            createNewConfigContent(project);
+            return;
+        }
+
+
         ContentManager contentManager = toolWindow.getContentManager();
         contentManager.removeAllContents(true);
         ContentFactory factory = contentManager.getFactory();
@@ -40,4 +56,16 @@ public class ChatGPTCopilotToolWindowFactory implements ToolWindowFactory, DumbA
         toolWindow.setAnchor(ToolWindowAnchor.LEFT, null);
     }
 
+    private void createNewConfigContent(Project project) {
+        ContentManager contentManager = toolWindow.getContentManager();
+        contentManager.removeAllContents(true);
+        ContentFactory factory = contentManager.getFactory();
+        JBPanel holderPanel = ChatGPTCopilotPanelUtil.createPlaceHolderPanel("Please add chatGPT copilot API token");
+        //添加刷新按钮
+        JButton button = new JButton(AllIcons.General.Add);
+        button.addActionListener(event -> new ConfigureChatGptDialog(project).show());
+
+        holderPanel.add(button);
+        contentManager.addContent(factory.createContent(holderPanel, "Config", false));
+    }
 }
